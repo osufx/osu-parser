7d536773be61b873f8acad36f0d09af0361ef265
Sunpy <jonas_steinsvik@hotmail.no>
2017-11-22 16:11:56 +0100

    Sub-paths for bezier path

--- osu_parser/curves.py
+++ osu_parser/curves.py
@@ -8,8 +8,7 @@ class Linear(object):   #Because it made sense at the time...
 class Bezier(object):
     def __init__(self, points, use_anchorpoints = False):
         self.points = points
-        self.order = len(points)
-        self.step = 0.0025 / self.order
+        self.order = len(self.points)
         self.pos = []
         self.anchor = use_anchorpoints
         self.calc_points()
@@ -18,20 +17,31 @@ class Bezier(object):
         if len(self.pos) != 0:  #This should never happen but since im working on this I want to warn myself if I fuck up
             raise Exception("Bezier was calculated twice!")
 
+        sub_points = []
+        for point in self.points:
+            if len(sub_points) >= 2 and point.x == sub_points[-1].x and point.y == sub_points[-1].y:
+                self.bezier(sub_points)
+                sub_points.clear()
+
+            sub_points.append(point)
+
+    def bezier(self, points):
+        order = len(points)
+        step = 0.0025 / order #DEBUG: Change back to 0.0025
         i = 0
-        n = self.order - 1
-        while i < 1 + self.step:
+        n = order - 1
+        while i < 1 + step:
             x = 0
             y = 0
 
             for p in range(n + 1):
                 a = mathhelper.cpn(p, n) * pow((1 - i), (n - p)) * pow(i, p)
-                x += a * self.points[p].x
-                y += a * self.points[p].y
+                x += a * points[p].x
+                y += a * points[p].y
 
             point = mathhelper.Vec2(x, y)
             self.pos.append(point)
-            i += self.step
+            i += step
 
     def point_at_distance(self, length):
         return {
--- test_draw.py
+++ test_draw.py
@@ -9,15 +9,18 @@ tur.penup()
 tur.speed(10)
 
 for hitobject in beatmap.hitobjects:
+    if hitobject.time < 396203:
+        continue
     tur.goto(hitobject.x, -hitobject.y)
     tur.pendown()
     tur.pencolor("red")
     if 2 & hitobject.type:
+        tur.dot(6)
         for point in hitobject.path:
             tur.goto(point.x, -point.y)
     else:
         tur.pencolor("blue")
-        tur.forward(2)
+        tur.dot(6)
     tur.penup()
 
 wn.exitonclick()
