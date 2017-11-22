71bbc9ddfb3608d98f25aa2ac6be73d93ec2c55e
Sunpy <jonas_steinsvik@hotmail.no>
2017-11-22 14:43:27 +0100

    Very bad visual test

--- osu_parser/hitobject.py
+++ osu_parser/hitobject.py
@@ -1,3 +1,5 @@
+from osu_parser import mathhelper, curves
+
 class HitObject(object):
     def __init__(self, x, y, time, object_type, slider_type = None, curve_points = None, repeat = 1, pixel_length = 0, timing_point = None, difficulty = None):
         """
@@ -24,7 +26,7 @@ class HitObject(object):
         #isSlider?
         if 2 & self.type:
             self.slider_type = slider_type
-            self.curve_points = curve_points
+            self.curve_points = [mathhelper.Vec2(self.x, self.y)] + curve_points
             self.repeat = repeat
             self.pixel_length = pixel_length
 
@@ -32,3 +34,50 @@ class HitObject(object):
             self.timing_point = timing_point
             self.difficulty = difficulty
 
+            self.calc_slider()
+    
+    def calc_slider(self):
+        if self.slider_type == "P" and len(self.curve_points) > 3:
+            self.slider_type = "B"
+        elif len(self.curve_points) == 2:
+            self.end = mathhelper.point_on_line(self.curve_points[0], self.curve_points[1], self.pixel_length)
+
+
+        #Make path
+        if self.slider_type == "L":     #Linear
+            path = curves.Linear(self.curve_points).pos
+        elif self.slider_type == "P":   #Perfect
+            curve = curves.Perfect(self.curve_points)
+            path = []
+            l = 0
+            step = 0.5
+            while l <= self.pixel_length:
+                path.append(curve.point_at_distance(l))
+                l += step
+        elif self.slider_type == "B":   #Bezier
+            curve = curves.Bezier(self.curve_points, True)
+            path = curve.pos
+        elif self.slider_type == "C":   #Catmull
+            curve = curves.Catmull(self.curve_points)
+            path = curve.pos
+        else:
+            raise Exception("Slidertype not supported! ({})".format(self.slider_type))
+
+        #Make end
+        if not hasattr(self, 'end'):
+            if self.slider_type == "L":     #Linear
+                self.end = mathhelper.point_on_line(path[0], path[1], self.pixel_length)
+            elif self.slider_type == "P":   #Perfect
+                self.end = curve.point_at_distance(self.pixel_length)
+            elif self.slider_type == "B":   #Bezier
+                self.end = curve.point_at_distance(self.pixel_length)
+            elif self.slider_type == "C":   #Catmull
+                self.end = curve.point_at_distance(self.pixel_length)
+            else:
+                raise Exception("Slidertype not supported! ({})".format(self.slider_type))
+
+        self.path = path
+
+        #Place slider_ticks
+
+        #Place end_point
--- test_draw.py
+++ test_draw.py
@@ -1,9 +1,23 @@
-from turtle import *
+import turtle
 from osu_parser.beatmap import Beatmap
 
 beatmap = Beatmap("test.osu")
 
-for hitobject in beatmap.hitobjects:
-    plot.scatter(hitobject.x,hitobject.y)
+wn = turtle.Screen()
+tur = turtle.Turtle()
+tur.penup()
+tur.speed(10)
 
-pyplot.show()
+for hitobject in beatmap.hitobjects:
+    tur.goto(hitobject.x, -hitobject.y)
+    tur.pendown()
+    tur.pencolor("red")
+    if 2 & hitobject.type:
+        for point in hitobject.path:
+            tur.goto(point.x, -point.y)
+    else:
+        tur.pencolor("blue")
+        tur.forward(2)
+    tur.penup()
+
+wn.exitonclick()
